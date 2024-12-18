7abefde7e5e84beff457244d6d5ed8df5e48dbd4
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-12-18 23:38:20 +0400

    add already data check

--- src/add_data.py
+++ src/add_data.py
@@ -13,6 +13,10 @@ import asyncio
 
 
 async def main():
+    async with async_session_maker() as session:
+        test = await RespondentsRepository(session).query_audience("where id > 0 and id < 2")
+        if test:
+            return
     df = pd.read_csv("data 1.csv")
     lists = [j[0].split(";") for j in df.values.tolist()]
     data = [SRespondents(id = int(record[0]), Date=str(record[1]), respondent=int(record[2]), Sex=int(record[3]), Age=int(record[4]), Weight=float(record[5])) for record in lists]
